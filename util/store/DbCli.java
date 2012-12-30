1452dea83839687201a3cada15f1afb18cdcc701
pvto <paavo.v.toivanen@gmail.com>
2012-12-30 23:31:33 +0200

    refactoring

--- util/store/DbCli.java
+++ util/store/DbCli.java
@@ -5,22 +5,25 @@ import java.sql.SQLException;
 
 public interface DbCli {
     
-    public Connection getConnection() throws SQLException;
-    public void close() throws SQLException;
-    public void execute(String sql) throws SQLException;
-    public Object[] execute(String sql, String[] generatedIdCols) throws SQLException;
-    public Q query(String sql) throws SQLException;
-    public Object[] singleResult(Object[] into, String sql) throws SQLException;            
-    public int count(Q q) throws SQLException;
-    public int count(String tableName, String where) throws SQLException;    
+    public Connection   getConnection()                                 throws SQLException;
+    public void         close()                                         throws SQLException;
+    public void         execute(String sql)                             throws SQLException;
+    public Object[]     execute(String sql, String[] generatedIdCols)   throws SQLException;
+    public Q            query(String sql)                               throws SQLException;
+    public Object[]     singleResult(Object[] into, String sql)         throws SQLException;            
+    public int          count(Q q)                                      throws SQLException;
+    public int          count(String tableName, String where)           throws SQLException;    
 
     
-    public static class x {
+    public static class x
+    {
         private static DbClimp cli = null;
-        private static DbClimp _cli(String jdbcUrl) {
+        private static DbClimp _cli(String jdbcUrl)
+        {
             return cli == null ? (cli = new DbClimp(jdbcUrl)) : cli;
         }
-        public static DbCli cli() {
+        public static DbCli cli()
+        {
             return _cli("jdbc:hsqldb:hsql://localhost:9001/neur");
         }
     }
